e40d44ffe08fb196c17c153ba177383bd6f6adb1
j-hannes <hannes_e@gmx.de>
2016-10-02 20:54:48 +0200

    Remove dev parts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router'
 import NavigationBar from './NavigationBar'
 import FlashMessageList from './flash/FlashMessageList'
 
@@ -9,10 +8,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <ul>
-          <li><Link to="/signup" activeStyle={{ color: 'red' }}>Signup</Link></li>
-          <li><Link to="/login" activeStyle={{ color: 'red' }}>Login</Link></li>
-        </ul>
         <NavigationBar />
         <FlashMessageList />
         {this.props.children}
